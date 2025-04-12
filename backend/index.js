3c41c931bf4573b9c3ea0efac255c95dc0328b52
harsh <rockingharsh305@gmail.com>
2025-04-12 16:30:04 +0530

    cors2

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors()); 
 app.use(cors({
-    origin: 'https://bookstore-phi-nine.vercel.app/', // frontend origin
+    origin: 'https://bookstore-phi-nine.vercel.app', // frontend origin
     credentials: true // if you're using cookies/auth
   }));
 app.use(express.json()); 
